5b5e0a73eaba6b55cd0ddf6239818671e48f4e74
Alumno <sgsdg>
2022-07-08 19:01:18 +0200

    Commit segundo. Insercion de los vector asset al lado de los edit text e insercion de menu en la primera activity

--- app/src/main/java/com/example/signup/DataActivity.kt
+++ app/src/main/java/com/example/signup/DataActivity.kt
@@ -2,13 +2,32 @@ package com.example.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.signup.databinding.ActivityDataBinding
 
 class DataActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDataBinding
+    companion object{
+        private val ID_CONFIRMACION = Menu.FIRST
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val actionBar = supportActionBar
+
+        actionBar!!.title = "  Sign Up"
+
+        actionBar.setDisplayUseLogoEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setDisplayUseLogoEnabled(true)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add(Menu.NONE, DataActivity.ID_CONFIRMACION, Menu.NONE, "Confirmación")
+        menuInflater.inflate(R.menu.activity_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/main/java/com/example/signup/MainActivity.kt
+++ app/src/main/java/com/example/signup/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.example.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.signup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    companion object{
+        private val ID_NUEVO_GRUPO = Menu.FIRST
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val actionBar = supportActionBar
+
+        actionBar!!.title = "  Sign Up"
+
+        actionBar.setDisplayUseLogoEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setDisplayUseLogoEnabled(true)
+
+        binding.btnRegister.setOnClickListener {
+            val myIntent = Intent(this, DataActivity::class.java)
+            startActivity(myIntent)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add(Menu.NONE, ID_NUEVO_GRUPO, Menu.NONE, "Nuevo grupo")
+        menuInflater.inflate(R.menu.activity_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
